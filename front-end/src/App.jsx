e68c260ebfa5300cd3594a6e36120ff82d07ab86
EnzzoNatan <enzzoferrazz1909@gmail.com>
2025-06-18 05:24:50 -0300

    correção do bug das rotas

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/forgot-password" element={<ForgotPassword/>}/>
+        <Route path="/ForgotPassword" element={<ForgotPassword/>}/>
       </Routes>
       
     </BrowserRouter>
